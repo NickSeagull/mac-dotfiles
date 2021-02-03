6db80b953b6d8bb542a08ff6e3bb66c370593cf3
Nikita Tchayka <nick@booster.cloud>
2021-02-03 11:46:41 +0000

    February 3, 2021 11:46 AM

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -30,3 +30,7 @@ prompt pure
 # Aliases
 alias brew-reload='(cd ~/.dotfiles/brew && brew bundle)'
 if [ -e /Users/nick/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/nick/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion

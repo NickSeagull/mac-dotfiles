ce5bea3e0526cdadd5e8622abd44b6e163a4dcfc
Nikita Tchayka <nick@booster.cloud>
2021-03-17 12:09:19 +0000

    Update

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -1,15 +1,10 @@
 
 #!/bin/bash
+
+npm config set registry http://localhost:4873
 COLOR='\033[0;32m'
 publish(){
   declare -a packages=("${!1}")
-  #for path in "${packages[@]}"
-  #do
-  #  echo -e "${COLOR} ⏳ Compiling $(basename "$path") $(tput sgr0)"
-  #  cd $path || exit
-  #  yarn --silent compile || exit
-  #  cd ../..
-  #done
   for path in "${packages[@]}"
   do
     echo -e "${COLOR} ✨🧙✨ Doing some magic to publish $(basename "$path") $(tput sgr0)"
@@ -33,26 +28,16 @@ PACKAGES+=(./packages/framework-provider-local-infrastructure)
 PACKAGES+=(./packages/framework-provider-azure)
 PACKAGES+=(./packages/framework-provider-kubernetes)
 PACKAGES+=(./packages/framework-provider-kubernetes-infrastructure)
-PACKAGES+=(./packages/rocket-static-sites-aws-infrastructure)
-PACKAGES+=(./packages/rocket-uploads3-store-event-aws-infrastructure)
 PACKAGES+=(./packages/cli)
 publish PACKAGES[@]
 echo -e "${COLOR} 🧨  Uninstalling the current Booster version $(tput sgr0)"
 npm uninstall -g @boostercloud/cli
 echo -e "${COLOR} 💾  Installing the local Booster version including your magic  $(tput sgr0)"
 npm install -g @boostercloud/cli
-if [ $# -eq 1 ]
-then
-  rm -rf $1
-  mkdir $1
-  cd $1
-  boost new:project test
-  cd test
-  cp -r ../../rdiaz/populateTestProject/* ./src/
-  STARTTIME=$(date +%s)
-  boost deploy -e production
-  ENDTIME=$(date +%s)
-fi
+npm config set registry https://registry.npmjs.org
+
+ENDTIME=$(date +%s)
+
 echo -e "${COLOR} -----------------------"
 echo -e "| 🧙 Deploy time: $(($ENDTIME - $STARTTIME)) s |"
 echo -e " ----------------------- $(tput sgr0)"
